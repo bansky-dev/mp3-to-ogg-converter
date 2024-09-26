50a83d7954b889a6c9dd459c5dc26d936fdabd35
Kociarz <dbednorz@tuta.io>
2024-09-26 21:40:25 +0200

    first commit

---
+++ mp3-to-ogg-rework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NAudio;
+using NVorbis;
+
+namespace mp3_to_ogg_rework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hey");
+    }
+}
