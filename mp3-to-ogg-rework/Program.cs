e9116175a313d5a44f82f844e78a4902c91c7d6d
Kociarz <dbednorz@tuta.io>
2024-09-27 23:16:48 +0200

    Made most basic code

--- mp3-to-ogg-rework/Program.cs
+++ mp3-to-ogg-rework/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using NAudio;
+using System.IO;
+using NAudio.Wave;
 using NVorbis;
+
 
 namespace mp3_to_ogg_rework;
 
@@ -8,6 +10,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("Hey");
+        string mp3FilePath = "input.mp3";  // Path to your MP3 file
+        string pcmOutputPath = "output.pcm";  // Path for the PCM output file
+
+        DecodeMp3ToPcm(mp3FilePath, pcmOutputPath);
+
     }
+
+    static void DecodeMp3ToPcm(string mp3FilePath, string pcmOutputPath)
+    {
+        using (var mp3Reader = new Mp3FileReader(mp3FilePath))
+        using (var pcmStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader))
+        using (var outputStream = new FileStream(pcmOutputPath, FileMode.Create, FileAccess.Write))
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = pcmStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                outputStream.Write(buffer, 0, bytesRead);
+            }
+        }
+
+        Console.WriteLine("MP3 has been successfully decoded to PCM!");
+    }
+
 }
