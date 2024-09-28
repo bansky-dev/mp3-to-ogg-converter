3ac54007fbc62520d8169485af340fa75e60fbe3
Kociarz <dbednorz@tuta.io>
2024-09-28 10:18:19 +0200

    Added sketch for encoding raw file into OGG

--- mp3-to-ogg-rework/Program.cs
+++ mp3-to-ogg-rework/Program.cs
@@ -8,11 +8,13 @@ namespace mp3_to_ogg_rework;
 class Program
 {
     static string mp3FilePath;
-    static string pcmOutputPath;
+    static string pcmFilePath;
     static bool isLooped = true;
     static void Main(string[] args)
     {
         while(isLooped){
+
+            //TODO: Change everything from pcm to ogg
 
             menuHandler();
 
@@ -22,9 +24,10 @@ class Program
                 System.Console.Write("Choose an MP3 file path (full path): ");
                 mp3FilePath = Console.ReadLine();
                 System.Console.Write("Choose an PCM file path (full path): ");
-                pcmOutputPath = Console.ReadLine();
+                pcmFilePath = Console.ReadLine();
                 
-                decodeMp3ToPcm(mp3FilePath, pcmOutputPath);
+                decodeMp3ToPcm(mp3FilePath, pcmFilePath);
+                encodePcmToOgg();
             }
             else if(choice == 2)
             {
@@ -38,6 +41,7 @@ class Program
     static void decodeMp3ToPcm(string mp3FilePath, string pcmOutputPath)
     {
         try{
+            System.Console.WriteLine("Deocing MP3 to raw PCM file...");
             using (var mp3Reader = new Mp3FileReader(mp3FilePath))
             using (var pcmStream = WaveFormatConversionStream.CreatePcmStream(mp3Reader))
             using (var outputStream = new FileStream(pcmOutputPath, FileMode.Create, FileAccess.Write))
@@ -56,6 +60,32 @@ class Program
             System.Console.WriteLine($"Error occured during decoding! {ex}");
         }
         
+    }
+
+
+    //TODO: Organize these datatypes and entire code;
+    static void encodePcmToOgg(string pcmPath){
+        var oggStream = new FileStream(oggOutputPath, FileMode.Create);
+        var vorbisEncoder = new OggVorbisEncoder.OggEncoder();
+
+        using (var pcmStream = new FileStream(pcmFilePath, FileMode.Open, FileAccess.Read))
+        {
+            var pcmWaveFormat = new WaveFormat(sampleRate, 16, channels);
+            var waveProvider = new RawSourceWaveStream(pcmStream, pcmWaveFormat);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = waveProvider.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                // Encode the PCM data to OGG using the encoder
+                vorbisEncoder.WriteSamples(buffer, bytesRead, channels, sampleRate);
+            }
+
+            // Finalize the encoding and close the stream
+            vorbisEncoder.Close();
+        }
+
+        Console.WriteLine("PCM has been successfully encoded to OGG!");
     }
 
     static void menuHandler(){
